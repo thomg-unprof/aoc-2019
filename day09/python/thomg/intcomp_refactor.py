a0f8daef3769a42487a9bfaa4bb9225081b53b01
Thomas Grosser <58363066+thomgro@users.noreply.github.com>
2020-10-09 14:20:59 +0200

    intcomp and tests on level with day02

--- day09/python/thomg/intcomp_refactor.py
+++ day09/python/thomg/intcomp_refactor.py
@@ -2,6 +2,7 @@ import sys
 
 class integerComputer:
     program = []
+    pointer = 0
 
 
     def readProgramFile(self, filename):
@@ -11,6 +12,48 @@ class integerComputer:
         self.program = programString[0].split(",")
         self.program = list(map(int, self.program))
 
+    def add(self):
+        arg1 = self.program[ self.program[ self.pointer+1 ] ]
+        arg2 = self.program[ self.program[ self.pointer+2 ] ]
+        sum = arg1 + arg2
+        targetAddress = self.program[ self.pointer+3 ]
+        self.program[ targetAddress ] = sum
+        self.pointer += 4
+
+    def multiply(self):
+        arg1 = self.program[ self.program[ self.pointer+1 ] ]
+        arg2 = self.program[ self.program[ self.pointer+2 ] ]
+        product = arg1 * arg2
+        targetAddress = self.program[ self.pointer+3 ]
+        self.program[ targetAddress ] = product
+        self.pointer += 4
+
     def run(self):
+        while 1:
+            instruction = self.program[ self.pointer ]
+            opcode = instruction % 100
+            #instruction -= opcode
+            #mode1 = (instruction % 1000)   / 100
+            #instruction -= mode1*100
+            #mode2 = (instruction % 10000)  / 1000
+            #instruction -= mode2*1000
+            # mode for the third parameter isn't actually needed on day05
+            # all opcodes so far HAVE to be in position mode for their third param, if they have one
+            #mode3 = (instruction % 100000) / 10000
+            modes = []
+
+            if opcode == 1:
+                self.add()
+            
+            elif opcode == 2:
+                self.multiply()
+            
+            elif opcode == 99:
+                break
+        
+        print(str(self.program))
         return 0
 
+intcomp = integerComputer()
+intcomp.readProgramFile("program_day02.txt")
+intcomp.run()
--- day09/python/thomg/test_intcomp.py
+++ day09/python/thomg/test_intcomp.py
@@ -4,10 +4,23 @@ from intcomp_refactor import integerComputer
 
 @pytest.fixture
 def intcomp():
+    """intcomp with empty program"""
     return integerComputer()
+
 
 
 def test_readProgramFile(intcomp):
     intcomp.readProgramFile("testfile.txt")
     assert intcomp.program == [1,2,3,4,7]
 
+def test_add(intcomp):
+    intcomp.program = [1,4,5,6,35,12,0]
+    intcomp.add()
+    assert intcomp.program == [1,4,5,6,35,12,47]
+    assert intcomp.pointer == 4
+
+def test_multiply(intcomp):
+    intcomp.program = [1,4,5,6,6,12,0]
+    intcomp.multiply()
+    assert intcomp.program == [1,4,5,6,6,12,72]
+    assert intcomp.pointer == 4
